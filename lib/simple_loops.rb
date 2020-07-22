63f899c1af6b9f5149db99fedaee476fa8a66e14
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-22 07:55:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,10 @@
 # Write your methods here
 def loop_message_five_times(message)
-  5.Time{puts message}
+  5.times{puts message}
 end
 
 def loop_message_n_times(message, n)
-  n.Times{puts message}
+  n.times{puts message}
 end
 
 def output_array(array)
